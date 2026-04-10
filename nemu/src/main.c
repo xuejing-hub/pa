1aa3829c35cd43964878362926b2ee0fc9c5267d
tracer-ics2017 <tracer@njuics.org>
2026-04-10 04:00:51 -0700

    >  compile 2312175 xijie Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  04:00:51 up 30 min,  1 user,  load average: 1.67, 1.17, 1.02 b4c6a1177ecaa1bce83e1d65288020819f35f92c

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -2,11 +2,12 @@ int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
+  /* 使用命令行参数初始化监控程序，决定是否进入批处理模式 */
   int is_batch_mode = init_monitor(argc, argv);
 
-  /* Receive commands from user. */
+  /* 进入命令处理主循环；批处理模式下会在执行完命令后退出 */
   ui_mainloop(is_batch_mode);
 
+  /* 正常退出 */
   return 0;
 }
