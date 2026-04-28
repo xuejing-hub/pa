cd1e1f414f2933bc73bd21e5a1a6aac859c79305
tracer-ics2017 <tracer@njuics.org>
2026-04-28 01:12:05 -0700

    >  run 2312175 xijie Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:12:05 up 20 min,  1 user,  load average: 0.59, 0.47, 0.66 8fb66f14952517de73937735f4d6bb8a115ac7

--- nexus-am/apps/litenes/src/psg.c
+++ nexus-am/apps/litenes/src/psg.c
@@ -10,16 +10,30 @@ static inline byte key_down(int key) {
     return key_state[key] ? 1 : 0;
 }
 
+// static inline byte pad1_button(int idx) {
+//     switch (idx) {
+//         case 1: return key_down(_KEY_G) | key_down(_KEY_J) | key_down(_KEY_SPACE);      // A
+//         case 2: return key_down(_KEY_H) | key_down(_KEY_K) | key_down(_KEY_LCTRL);      // B
+//         case 3: return key_down(_KEY_T) | key_down(_KEY_TAB);                             // SELECT
+//         case 4: return key_down(_KEY_Y) | key_down(_KEY_RETURN);                          // START
+//         case 5: return key_down(_KEY_W) | key_down(_KEY_UP);                              // UP
+//         case 6: return key_down(_KEY_S) | key_down(_KEY_DOWN);                            // DOWN
+//         case 7: return key_down(_KEY_A) | key_down(_KEY_LEFT);                            // LEFT
+//         case 8: return key_down(_KEY_D) | key_down(_KEY_RIGHT);                           // RIGHT
+//         default: return 0;
+//     }
+// }
+
 static inline byte pad1_button(int idx) {
     switch (idx) {
-        case 1: return key_down(_KEY_G) | key_down(_KEY_J) | key_down(_KEY_SPACE);      // A
-        case 2: return key_down(_KEY_H) | key_down(_KEY_K) | key_down(_KEY_LCTRL);      // B
-        case 3: return key_down(_KEY_T) | key_down(_KEY_TAB);                             // SELECT
-        case 4: return key_down(_KEY_Y) | key_down(_KEY_RETURN);                          // START
-        case 5: return key_down(_KEY_W) | key_down(_KEY_UP);                              // UP
-        case 6: return key_down(_KEY_S) | key_down(_KEY_DOWN);                            // DOWN
-        case 7: return key_down(_KEY_A) | key_down(_KEY_LEFT);                            // LEFT
-        case 8: return key_down(_KEY_D) | key_down(_KEY_RIGHT);                           // RIGHT
+        case 1: return key_down(_KEY_G);                                                  // A
+        case 2: return key_down(_KEY_H);                                                  // B
+        case 3: return key_down(_KEY_T);                                                  // SELECT
+        case 4: return key_down(_KEY_Y);                                                  // START
+        case 5: return key_down(_KEY_W);                                                  // UP
+        case 6: return key_down(_KEY_S);                                                  // DOWN
+        case 7: return key_down(_KEY_A);                                                  // LEFT
+        case 8: return key_down(_KEY_D);                                                  // RIGHT
         default: return 0;
     }
 }
